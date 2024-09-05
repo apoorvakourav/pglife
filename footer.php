08e92aab61cd06e830cde19d54893c583578e034
apoorvakourav <kouravapoorva@gamil.com>
2024-09-05 16:43:40 +0530

    all files are added

---
+++ footer.php
@@ -0,0 +1,21 @@
+div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+<div class="footer-city">
+    <a href="property_list.php?city=Delhi">PG in Delhi</a>
+</div>
+<div class="footer-city">
+    <a href="property_list.php?city=mumbai">PG in Mumbai</a>
+</div>
+<div class="footer-city">
+    <a href="property_list.php?city=Bengaluru">PG in Bengaluru</a>
+</div>
+<div class="footer-city">
+    <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+</div>
+        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/common.css" rel="stylesheet"/>
+    <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+    <link href="home.css/home.css" rel="stylesheet"/>
+  </head>
+<header>
+  
+    <!-- As a link -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="img/logo.png" alt=""></a>
+  </div>
+</nav>
+
+<!-- As a heading -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1"></span>
+  </div>
+  <div class="sticky-top">
+    <i class="fa-solid fa-user"></i>
+    <p>signup</p>
+      
+    </div>
+</nav>
+</header>
+<body>
+  <div id="body">
+    
+  </div>
+</body>
+
+</html>
